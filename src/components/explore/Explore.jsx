18f2a9a8c7bc008a895b45583aed25f84d52fda4
smoukeuur <enzotang@live.fr>
2023-10-15 20:48:25 +0200

    [development] development suite

--- src/components/explore/Explore.jsx
+++ src/components/explore/Explore.jsx
@@ -160,21 +160,19 @@ const Explore = () => {
     return tableau;
   }
 
-  const couleurs = melanger([
-    "rgba(150,0,0,1)",
-    "rgba(150,85,0,1)",
-    "rgba(130,133,20,1)",
-    "rgba(60,130,55,1)",
-    "rgba(50,130,120,1)",
-    "rgba(40,120,130,1)",
-    "rgba(20,100,150,1)",
-    "rgba(110,50,140,1)",
-    "rgba(150,0,130,1)",
-    "rgba(150,55,55,1)",
-  ]);
+  const gradients = melanger([
+    "linear-gradient(45deg, rgba(126, 139, 252, 1), rgba(23, 26, 32, 1))",
+    "linear-gradient(45deg, rgba(113, 128, 242, 1), rgba(20, 23, 30, 1))",
+    "linear-gradient(45deg, rgba(139, 152, 262, 1), rgba(26, 29, 35, 1))",
+    "linear-gradient(45deg, rgba(120, 133, 247, 1), rgba(22, 25, 31, 1))",
+    "linear-gradient(45deg, rgba(134, 147, 257, 1), rgba(25, 28, 34, 1))",
+    "linear-gradient(45deg, rgba(129, 142, 252, 1), rgba(24, 27, 33, 1))",
+    "linear-gradient(45deg, rgba(141, 154, 264, 1), rgba(27, 30, 36, 1))",
+    "linear-gradient(45deg, rgba(118, 131, 245, 1), rgba(21, 24, 29, 1))"
+]);
 
-  function obtenirCouleurAleatoire(indice) {
-    return couleurs[indice % couleurs.length];
+  function obtenirGradientAleatoire(indice) {
+    return gradients[indice % gradients.length];
   }
 
   return (
@@ -312,7 +310,7 @@ const Explore = () => {
               <div
                 key={index}
                 className="genre-card"
-                style={{ background: obtenirCouleurAleatoire(index) }}
+                style={{ background: obtenirGradientAleatoire(index) }}
               >
                 <p>{genre}</p>
               </div>
